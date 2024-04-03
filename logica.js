6c632560fa12c934674e50ad59b23a59cea67abd
jean luc Boza <lucmartinsjean@gmail.com>
2024-04-03 20:51:05 -0300

    commit exercicio1

---
+++ logica.js
@@ -0,0 +1,23 @@
+console.log("escreve no console!");
+//var idade = prompt("Qual a sua idade?");
+var idade = 20;
+if(idade >= 18) 
+{
+    console.log("É maior de idade");
+}
+else
+{
+    console.log("Não é maior de idade");
+}
+
+var num1 = prompt("digite o primeiro numero");
+var num2 = prompt("digite o segundo número");
+
+if (num1>num2)
+{
+    console.log("o número 1 e maior");
+}
+else
+{
+    console.log("o número 2 é o maior");
+}
