accf3699b72dd82c193ce6ff80c82484a40d1e00
jean luc Boza <lucmartinsjean@gmail.com>
2024-04-02 21:33:41 -0300

    commit aulasJS

---
+++ aula1.js
@@ -0,0 +1,6 @@
+var nome = "Jean"; // texto -> string
+var idade = 23; // número -> number -> int 
+var peso = 87.8; // number -> float
+var acordado = true; // boolean -> bool 
+//js é fracamente tipado
+//js é interpretado
